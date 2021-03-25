4976569905bd305535a5c613b92eb8b62628fffa
Echora-1 <51864493+Echora-1@users.noreply.github.com>
2021-03-25 15:32:32 +0300

    Настроил показ полноэкранного изображения с количеством лайков и комментариев

--- js/image-constructor.js
+++ js/image-constructor.js
@@ -2,38 +2,18 @@ import {isEscPressed, isClick, removeСhildByClass} from './utils.js';
 
 const COMMENTS_PER_UPLOAD = 5;
 const bigImageContainerElement = document.querySelector('.big-picture');
-const bigPicturePreviewElement = document.querySelector('.big-picture__preview');
 const bigImageCancelElement = document.querySelector('.big-picture__cancel');
 const templateImageElement = document.querySelector('#picture').content.querySelector('.picture');
 const bigImageElement = document.querySelector('.big-picture__img').querySelector('img');
 const descriptionImageElement = document.querySelector('.social__caption');
 const likesCountElement = document.querySelector('.likes-count');
 const socialCommentContainerElement = document.querySelector('.social__comment-count');
-const socialComments = document.querySelector('.social__comments');
-const socialComment = document.querySelector('.social__comment');
-const commentsLoader = document.querySelector('.comments-loader');
-
-const closeBigImage = () => {
-  document.body.classList.remove('modal-open');
-  bigImageContainerElement.classList.add('hidden');
-  bigImageCancelElement.removeEventListener('click', (evt) => onBigImageClosedСlickedСancel(evt));
-  document.removeEventListener('keydown', (evt) => onBigImageClosedPressedEsc(evt));
-}
-
-const onBigImageClosedСlickedСancel = (evt) => {
-  if(isClick(evt)) {
-    closeBigImage();
-  }
-}
-
-const onBigImageClosedPressedEsc = (evt) => {
-  if(isEscPressed(evt)) {
-    closeBigImage();
-  }
-}
+const socialCommentsElement = document.querySelector('.social__comments');
+const socialCommentElement = document.querySelector('.social__comment');
+const commentsLoaderElement = document.querySelector('.comments-loader');
 
 const createComment = (data) => {
-  const comment = socialComment.cloneNode(true);
+  const comment = socialCommentElement.cloneNode(true);
 
   comment.querySelector('.social__picture').src = data.avatar;
   comment.querySelector('.social__picture').alt = data.name;
@@ -43,10 +23,30 @@ const createComment = (data) => {
 }
 
 const onImageClick = (evt, src, description, likes,comments) => {
-  let arrayComments = Array.from(comments);
+  const arrayComments = Array.from(comments);
   let numberCurrentComments = comments.length > COMMENTS_PER_UPLOAD ? COMMENTS_PER_UPLOAD : comments.length;
 
   evt.preventDefault();
+
+  const closeBigImage = () => {
+    document.body.classList.remove('modal-open');
+    bigImageContainerElement.classList.add('hidden');
+    bigImageCancelElement.removeEventListener('click', (evt) => onBigImageClosedСlickedСancel(evt));
+    document.removeEventListener('keydown', (evt) => onBigImageClosedPressedEsc(evt));
+    commentsLoaderElement.removeEventListener('click', onLoaderClick);
+  }
+
+  const onBigImageClosedСlickedСancel = (evt) => {
+    if(isClick(evt)) {
+      closeBigImage();
+    }
+  }
+
+  const onBigImageClosedPressedEsc = (evt) => {
+    if(isEscPressed(evt)) {
+      closeBigImage();
+    }
+  }
 
   document.body.classList.add('modal-open');
   bigImageContainerElement.classList.remove('hidden');
@@ -59,30 +59,35 @@ const onImageClick = (evt, src, description, likes,comments) => {
   socialCommentContainerElement.textContent = `${numberCurrentComments} из ${arrayComments.length} комментариев`;
   likesCountElement.textContent = likes;
 
-  commentsLoader.classList.remove('hidden');
-  if(numberCurrentComments === comments.length) {
-    commentsLoader.classList.add('hidden');
+  commentsLoaderElement.classList.remove('hidden');
+  if(numberCurrentComments === arrayComments.length) {
+    commentsLoaderElement.classList.add('hidden');
   }
 
-  removeСhildByClass(socialComments, 'social__comment');
+  removeСhildByClass(socialCommentsElement, 'social__comment');
   for (let i = 0; i < COMMENTS_PER_UPLOAD && i < arrayComments.length; i++) {
-    socialComments.append(createComment(arrayComments[i]));
+    socialCommentsElement.append(createComment(arrayComments[i]));
   }
 
-  commentsLoader.addEventListener('click', (evt) => {
-    console.log(arrayComments)
-    console.log(numberCurrentComments)
-    evt.preventDefault()
-    for (let i = numberCurrentComments; i < numberCurrentComments + COMMENTS_PER_UPLOAD && i < arrayComments.length; i++) {
-      socialComments.append(createComment(arrayComments[i]));
+  const onLoaderClick = uploadСomments(numberCurrentComments, arrayComments);
+
+  commentsLoaderElement.addEventListener('click', onLoaderClick);
+
+};
+
+const uploadСomments = (currentComments, comments) => {
+  return () => {
+    for (let i = currentComments; i <currentComments + COMMENTS_PER_UPLOAD && i < comments.length; i++) {
+      socialCommentsElement.append(createComment(comments[i]));
       if( i === comments.length - 1) {
-        commentsLoader.classList.add('hidden');
+        commentsLoaderElement.classList.add('hidden');
       }
     }
-    numberCurrentComments = numberCurrentComments + COMMENTS_PER_UPLOAD < arrayComments.length ? numberCurrentComments + COMMENTS_PER_UPLOAD : arrayComments.length;
-    socialCommentContainerElement.textContent = `${numberCurrentComments} из ${arrayComments.length} комментариев`;
-  })
+    currentComments = currentComments + COMMENTS_PER_UPLOAD < comments.length ? currentComments + COMMENTS_PER_UPLOAD : comments.length;
+    socialCommentContainerElement.textContent = `${currentComments} из ${comments.length} комментариев`;
+  }
 };
+
 
 const createImage = (dataItem) => {
   const image = templateImageElement.cloneNode(true);
